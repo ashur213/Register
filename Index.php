4e358f941dbec252875b759a821e4dd946666e9a
Ashur213 <ashur7@live.se>
2018-10-24 14:22:10 +0200

    Jag är klar med formulär

---
+++ Index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+        REGISTER:<br>
+        <form action="submit.php" method="POST">
+
+            Namn: <input type="text" name="name"><br>
+            Födelsedatum: <input type="date" name="birth"><br>
+            Mail: <input type="email" name="mail"><br>
+            Adress: <input type="text" name="address"><br>
+            Lösenord: <input type="password" name="password">
+    </form>
+</body>
+
+</html>
